e71c9f92bbde42758260dbaaa78fa4f6eeb52e83
birmansahil <contact.birmansahil@gmail.com>
2024-10-08 12:41:36 -0400

    learnt conditional statements

---
+++ script.js
@@ -0,0 +1,21 @@
+let firstCard = 6;
+let secondCard = 9;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
+let sum = firstCard + secondCard;
+
+if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+} else if (sum === 21) {
+    message = "You've got blackjack!";
+    hasBlackJack = true;
+} else {
+    isAlive = false;
+    message = "You're out of the game :(";
+}
+
+console.log(isAlive);
+console.log(hasBlackJack);
+console.log(message);
