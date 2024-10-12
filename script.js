228dd8b414d213b919d00cce5f1e81678b0887a6
birmansahil <contact.birmansahil@gmail.com>
2024-10-11 21:36:44 -0400

    added start game and draw card

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 let firstCard = 6;
 let secondCard = 9;
+let cards = [firstCard, secondCard];
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
 let sum = firstCard + secondCard;
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card?";
-} else if (sum === 21) {
-    message = "You've got blackjack!";
-    hasBlackJack = true;
-} else {
-    isAlive = false;
-    message = "You're out of the game :(";
-}
-
-console.log(isAlive);
-console.log(hasBlackJack);
-console.log(message);
+let messageEl = document.querySelector("#message-el"); 
+let cardsEl = document.querySelector("#cards-el");
+let sumEl = document.querySelector("#sum-el");
 
 function startGame() {
-    console.log("Game started");
+    renderGame();
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+    sumEl.textContent = "Sum: " + sum;
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "You've got blackjack!";
+        hasBlackJack = true;
+    } else {
+        isAlive = false;
+        message = "You're out of the game :(";
+    }
+
+    messageEl.textContent = message;
+}
+
+function newCard() {
+    let card = 5;
+    sum += card;
+    renderGame();
 }
