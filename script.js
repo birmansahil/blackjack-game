cc0f981de570cd77391386f69bdfab382c52b63c
birmansahil <contact.birmansahil@gmail.com>
2024-10-15 20:17:09 -0400

    added for loop and arrays to print cards

--- script.js
+++ script.js
@@ -16,7 +16,11 @@ function startGame() {
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+    cardsEl.textContent = "Cards: ";
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " ";
+    }
+
     sumEl.textContent = "Sum: " + sum;
 
     if (sum <= 20) {
@@ -34,6 +38,7 @@ function renderGame() {
 
 function newCard() {
     let card = 5;
+    cards.push(card);
     sum += card;
     renderGame();
 }
